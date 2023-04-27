6a26477e4aefc165e1f51588c17e36f294de5196
AyahDweikat <dweikatayah99@gmail.com>
2023-04-27 22:10:56 +0300

    ticket 3 is done

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
-
-
-
-
-
-// let answer = document.getElementById("answer");
-// let submit = document.getElementById("sum")
-// submit.addEventListener("click", (e) => {
-//     let num1 = document.getElementById("num1").value;
-//     let num2 = document.getElementById("num2").value;
-//     e.preventDefault()
-//     sumProcess(num1, num2)
-
-// })
-// let num1, num2;
-// sumProcess(num1, num2)
+let negativeNums = [];
 function sumProcess(...numbers){
+    negativeNums=[];
     let _numbers  = numbers.map((item)=>{
         return convertingNums(item)
     })
-    // let _num1 = convertingNums(num1)
-    // let _num2 = convertingNums(num2)
+    if(negativeNums.length){
+        throw new Error(`Negatives Are Not Allowed (${[...negativeNums]})`);
+    }
     return addTwoNums(..._numbers);
 }
 const addTwoNums = (...numbers) =>  {
@@ -28,7 +15,10 @@ const addTwoNums = (...numbers) =>  {
 function convertingNums(num){
     if(num > 1000) return 0;
     if(num =="")return 0;
-    if(num <0) throw new Error("negatives not allowed");
+    if(num <0) {
+        negativeNums.push(num)
+        // 
+    }
     return Number(num)
 }
 module.exports = sumProcess;
--- main.test.js
+++ main.test.js
@@ -1,32 +1,31 @@
 const sumProcess = require('./main')
 
 test("Return sum for num1 =''" , ()=>{
-    expect(sumProcess("", 5)).toBe(5)
+    expect(sumProcess("", "5")).toBe(5)
 })
 test("Return sum for num2 =''" , ()=>{
-    expect(sumProcess(5, "")).toBe(5)
+    expect(sumProcess("5", "")).toBe(5)
 })
-
 test("Return sum for num1 >=1000" , ()=>{
-    expect(sumProcess(1001, 5)).toBe(5)
+    expect(sumProcess("1001", "5")).toBe(5)
 })
 test("Return sum for num2 >=1000" , ()=>{
-    expect(sumProcess(5, 1005)).toBe(5)
+    expect(sumProcess("5", "1005")).toBe(5)
 })
 test("Return sum for num2 >=1000" , ()=>{
-    expect(sumProcess(5, 5)).toBe(10)
+    expect(sumProcess("5", "5")).toBe(10)
 })
 test("Return sum for num2 >=1000" , ()=>{
-    expect(sumProcess(5,7,4,4,7,8,9,10,6)).toBe(60)
+    expect(sumProcess("5","7","4","4","7","8","9","10","6")).toBe(60)
 })
 
 test("Return sum for num1 <0" , ()=>{
     expect(() => {
-        sumProcess(6,-5)
-      }).toThrow();
+        sumProcess("6","-5")
+      }).toThrow("Negatives Are Not Allowed (-5)");
 })
 test("Return sum for num1 <0" , ()=>{
     expect(() => {
-        sumProcess(-6,5)
-      }).toThrow();
+        sumProcess("-6","5","-10")
+      }).toThrow("Negatives Are Not Allowed (-6,-10)");
 })
