2a71466fb84788b1012195d29439f004bacfed4f
AyahDweikat <dweikatayah99@gmail.com>
2023-04-28 13:18:47 +0300

    edit code as review on spread array and convert array from global to local

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-let negativeNums = [];
 function sumProcess(...numbers){
-    negativeNums=[];
-    let _numbers  = numbers.map((item)=>{
-        return convertingNums(item)
-    })
+    let negativeNums = [];
+    let _numbers  = numbers.map((item)=> convertingNums(item,negativeNums))
     if(negativeNums.length){
-        throw new Error(`Negatives Are Not Allowed (${[...negativeNums]})`);
+        throw new Error(`Negatives Are Not Allowed (${negativeNums})`);
     }
-    return addTwoNums(..._numbers);
+    return addTwoNums(_numbers);
 }
-const addTwoNums = (...numbers) =>  {
+const addTwoNums = (numbers) =>  {
     return numbers.reduce((sum, item)=> sum + item, 0)
 };
-function convertingNums(num){
+function convertingNums(num,negativeNums){
     if(num > 1000) return 0;
     if(num =="")return 0;
     if(num <0) {
